1c5462633c028c49991e9907fa3e45f048a02006
Jack Hughes <jack4.hughes@live.uwe.ac.uk>
2024-10-16 02:14:58 +0100

    added string output for controlling arm through serial and removed main loop from robot.py

--- Robot.py
+++ Robot.py
@@ -6,7 +6,7 @@ import yaml
 import numpy as np
 from numpy import sin, cos
 from Joint import Joint, RevoluteJoint, PrismaticJoint, create_joint_from_robot_config_dict
-from typing import List
+from typing import Collection, Union
 from time import sleep
 
 ANGLE_UNITS = "°"
@@ -14,17 +14,24 @@ DISTANCE_UNITS = "cm"
 TIME_UNITS = "ms"
 
 class Robot():
-    def __init__(self, name: str, joints: List[Joint]):
+    def __init__(self, name: str, joints: Collection[Union[RevoluteJoint, PrismaticJoint]]):
         self.name = name
         self.joints = joints
 
-    def print_angle_information(self, debug=False):
+    def print_joint_angle_information(self, debug=False):
         print(f"joint_name\t\tangle/ext({ANGLE_UNITS})\tPWM Value({TIME_UNITS})")
         for joint in self.joints:
             joint.print_table_view()
 
-    def update_joint_target_angle(self, new_angle):
-        """Updates the target angles of the joint to some new position. ens"""
+    def string_output(self):
+        #0 P 1500 #1 P 1500 #2 P 1500 #3 P 1500 #4 P 1500 #5 P 1500)
+        output = ""
+        for list_index, joint in enumerate(self.joints):
+           duty_cyle = joint.servo_motor.duty_cycle
+           output += f"#{list_index} P {duty_cyle} "
+        output += "\r"
+        
+        return output
 
 def create_joint_list_from_config_file(filename):
     joint_dicts = config_scripts.load_config_file_from_yaml(filename)
@@ -34,13 +41,5 @@ def create_joint_list_from_config_file(filename):
 if __name__ == "__main__":
     robot_joints = create_joint_list_from_config_file("Motor Config Files/robot_setup_config.yaml")
     robot = Robot("Vanessa", robot_joints)
-    try:
-        while True:
-            robot.joints[1].servo_motor.duty_cycle += 2
-            robot.print_angle_information()
-            sleep(0.3)
-            os.system('cls' if os.name == 'nt' else 'clear')
-    except KeyboardInterrupt:
-        print("______________________\nprocess terminated")
-    finally:
-        print(robot.print_angle_information())
+    string = robot.string_output()
+    print(string)
